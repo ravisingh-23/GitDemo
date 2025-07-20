8fdcf7c474c24cb178fe697b0635c5ee3f14d5c6
Ravi <ravisingh990723@gmail.com>
2025-07-20 16:27:33 +0530

    Second Commit

--- Java_Practice/src/Simple_Practice_Codes/Print_Statement.java
+++ Java_Practice/src/Simple_Practice_Codes/Print_Statement.java
@@ -4,6 +4,7 @@ public class Print_Statement {
 
 	public static void main(String[] args) {
 		System.out.println("Git practice currently going on");
+	    System.out.println("second commit");
 	}
 
 }
