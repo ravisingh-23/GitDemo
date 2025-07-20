926d6669226adfe70ddbc7e183687ae3ea830718
Ravi <ravisingh990723@gmail.com>
2025-07-20 16:34:41 +0530

    Third Commit

--- Java_Practice/src/Simple_Practice_Codes/Print_Statement.java
+++ Java_Practice/src/Simple_Practice_Codes/Print_Statement.java
@@ -5,6 +5,7 @@ public class Print_Statement {
 	public static void main(String[] args) {
 		System.out.println("Git practice currently going on");
 	    System.out.println("second commit");
+	    System.out.println("Third Commit");
 	}
 
 }
